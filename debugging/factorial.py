23163e5acf539f43ea45c9edab6e4b339c9e4dfd
mac <mac@SK4DYs-MacBook.local>
2025-05-05 15:38:07 +0100

    debug

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -2,8 +2,6 @@
 import sys
 
 def factorial(n):
-    if n < 0:
-        raise ValueError("Factorial is not defined for negative numbers")
     result = 1
     while n > 1:
         result *= n
@@ -11,13 +9,13 @@ def factorial(n):
     return result
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: ./factorial.py <number>")
-        sys.exit(1)
-
-    try:
-        number = int(sys.argv[1])
-        print(factorial(number))
-    except ValueError as e:
-        print("Error:", e)
+    if len(sys.argv) == 2:
+        try:
+            number = int(sys.argv[1])
+            if number < 0:
+                sys.exit(1)
+            print(factorial(number))
+        except Exception:
+            sys.exit(1)
+    else:
         sys.exit(1)
